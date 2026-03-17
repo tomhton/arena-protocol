63e8fb7cad7183cdf8bc16af3c565c362e4d27c3
Baloo <baloo@Ngocs-MacBook-Air.local>
2026-03-16 23:40:39 -0700

    fix: remove duplicate Color.init(hex:) in Live Activity widget

--- ios/ArenaProtocolWidget/ArenaProtocolWidgetLiveActivity.swift
+++ ios/ArenaProtocolWidget/ArenaProtocolWidgetLiveActivity.swift
@@ -225,23 +225,4 @@ private struct ExpandedBottomView: View {
     }
 }
 
-// MARK: - Color(hex:) helper (widget extension scope)
-// Duplicated here because widget extensions don't share the main app module.
 
-private extension Color {
-    init(hex: String) {
-        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
-        var int: UInt64 = 0
-        Scanner(string: hex).scanHexInt64(&int)
-        let r, g, b: Double
-        switch hex.count {
-        case 6:
-            r = Double((int >> 16) & 0xFF) / 255
-            g = Double((int >>  8) & 0xFF) / 255
-            b = Double( int        & 0xFF) / 255
-        default:
-            r = 1; g = 1; b = 1
-        }
-        self.init(red: r, green: g, blue: b)
-    }
-}
